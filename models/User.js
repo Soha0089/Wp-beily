55f539b41e2e19c02b503a2300b3b56e273f4a38
Soha0089 <mahmudx0010@gmail.com>
2025-11-18 23:08:52 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,15 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  name: { type: String, default: "" }, // ✅ নামের ডিফল্ট
-  coins: { type: Number, default: 0 },
-  exp: { type: Number, default: 0 },
-  level: { type: Number, default: 1 },
-  lastActive: { type: Number, default: Date.now },
+const groupSchema = new mongoose.Schema({
+  id: { type: String, required: true, unique: true },  
+
+  settings: {
+    welcomeDisabled: { type: Boolean, default: false },
+    welcomeMessage: { type: String, default: null },
+    goodbyeDisabled: { type: Boolean, default: false }
+  },
+
   commandCount: { type: Number, default: 0 },
-  lastDailyReward: { type: String, default: null },
-  joinDate: { type: Number, default: Date.now }
+
+  members: { type: Array, default: [] }
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.models.Group || mongoose.model("Group", groupSchema);
