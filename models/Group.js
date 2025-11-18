a182a53721d6729daf680f22ff3c1081e2f69867
Soha0089 <mahmudx0010@gmail.com>
2025-11-18 23:08:31 +0600

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -1,14 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
+  id: { type: String, required: true, unique: true },  
+
   settings: {
     welcomeDisabled: { type: Boolean, default: false },
     welcomeMessage: { type: String, default: null },
     goodbyeDisabled: { type: Boolean, default: false }
   },
+
   commandCount: { type: Number, default: 0 },
+
   members: { type: Array, default: [] }
 });
 
-module.exports = mongoose.model("Group", groupSchema);
+module.exports = mongoose.models.Group || mongoose.model("Group", groupSchema);
